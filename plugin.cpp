b1c0b4dcc3c955f92d9401e5e33e173189dd5bbe
Ashley Retallack <ashley.retallack@gmail.com>
2017-08-23 18:27:31 +0100

    split out plugin to headers and plugin

---
+++ plugin.cpp
@@ -0,0 +1,35 @@
+
+#include "gpuCacheTranslator.h"
+
+#include <extension/Extension.h>
+#include <maya/MTypes.h> 
+
+extern "C"
+{
+
+    DLLEXPORT void initializeExtension( CExtension &extension )
+    {
+        MStatus status;
+
+        const char * pluginVersion = "1.3.1";
+
+        MString info = "MTOA gpuCache Translator v";
+        info += pluginVersion;
+        info += " using ";
+        info += Alembic::AbcCoreAbstract::GetLibraryVersion().c_str();
+        MGlobal::displayInfo(info);
+
+        extension.Requires( "gpuCache" );
+        status = extension.RegisterTranslator( "gpuCache",
+                                               "",
+                                               GpuCacheTranslator::creator,
+                                               GpuCacheTranslator::nodeInitialiser);
+    }
+
+    DLLEXPORT void deinitializeExtension( CExtension &extension )
+    {
+    }
+
+} // extern "C"
+
+
